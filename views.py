52b292d0ca7c18d67b82b5082998c61c85ab8b75
devinshende <devin.s.shende@gmail.com>
2019-11-30 03:10:50 -0800

    changed user id code to be len(query all)a

--- views.py
+++ views.py
@@ -125,7 +125,7 @@ def signup():
 			print('half user is ',v)
 			print('half user\'s month is ',v['month'])
 			try:
-				id = User.query.all()[-1].id+1
+				id = len(User.query.all()[-1])
 			except IndexError:
 				# in the case that they are the first user, it throws a IndexError list index out of range
 				id = 0
